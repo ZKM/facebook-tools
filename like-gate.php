6de9d0195f3218a2532bce551b688bc4ccfec848
Zach Schneider <zschneider@zocalogroup.com>
2013-06-17 13:10:49 -0500

    selecting friend's ID

--- like-gate.php
+++ like-gate.php
@@ -16,15 +16,15 @@ $facebook = new Facebook(array(
 <html>
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <title>FB Tools</title>
+  <title>Coupon</title>
   <meta property="og:title" content=""/>
-  <meta property="og:type" content="contest"/>
-  <meta property="og:url" content="//www.zgresources.com/_facebook/zg_test_case/"/>
-  <meta property="og:image" content="/img/appLogo.jpg"/>
-  <meta property="og:site_name" content="Merci-O-Matic"/>
-  <meta property="fb:admins" content="100004990624658"/>
+  <meta property="og:type" content="sport"/>
+  <meta property="og:url" content="http://site.com"/>
+  <meta property="og:image" content="/images/appLogo.jpg"/>
+  <meta property="og:site_name" content="Coupon"/>
+  <meta property="fb:admins" content="USER_ID"/>
   <meta property="og:description" content="Description."/>
-  <meta property="fb:app_id" content="<?php echo $appId; ?>"/>
+  <meta property="fb:app_id" content="APP ID"/>
 </head>
 <body>
 <div id="fb-root"></div>
@@ -57,10 +57,34 @@ $facebook = new Facebook(array(
      ref.parentNode.insertBefore(js, ref);
    }(document, /*debug*/ false));
 </script>
-<pre>Your journey begins now.</pre>
-
 <?php
+function parse_signed_request($signed_request) {
+  list($encoded_sig, $payload) = explode('.', $signed_request, 2);
+
+  // decode the data
+  $sig  = base64_url_decode($encoded_sig);
+  $data = json_decode(base64_url_decode($payload), true);
+
+  if (strtoupper($data['algorithm']) !== 'HMAC-SHA256') {
+    error_log('Unknown algorithm. Expected HMAC-SHA256');
+    return null;
+  }
+  return $data;
+}
+
+function base64_url_decode($input) {
+  return base64_decode(strtr($input, '-_', '+/'));
+}
+
+if ($signed_request = parse_signed_request($_REQUEST['signed_request'])) {
+  if ($signed_request["page"]["liked"]) {
+    echo  ("<div id=\"like\"><pre>Your journey begins now.</pre></div>");
     include 'select_friend.php';
-?>
-</body>
-</html>
+
+  } else {
+    echo ("<div id=\"preLike\"><pre>To use this app you will need to like the page first</pre></div>");
+  }
+} else {
+  echo "No signed_request. I am broken.";
+}
+echo ("</body></html>");
--- select_friend.php
+++ select_friend.php
@@ -1,16 +1,132 @@
-<div>
-	<a class="friendSelect" href="#">Select a Friend</a>
+<link rel="stylesheet" href="./css/tdfriendselector.css?v=2" />
+
+<ul>
+	<li>Status: <span id="login-status">Not logged in</span> <a href="#" id="btnLogin">Login</a></li>
+	<li><a href="#" id="friendSelect">Select a friend</a> (You'll need to log in first)</li>
+</ul>
+
+<div id="results">
+	<h2><pre>ACTIVITY LOG</pre></h2>
 </div>
 
+<!-- Markup for These Days Friend Selector -->
+<div id="TDFriendSelector">
+	<div class="TDFriendSelector_dialog">
+		<a href="#" id="TDFriendSelector_buttonClose">x</a>
+		<div class="TDFriendSelector_form">
+			<div class="TDFriendSelector_header">
+				<p>Select your friend</p>
+			</div>
+			<div class="TDFriendSelector_content">
+				<p>Then you can invite them to join you in the app.</p>
+				<div class="TDFriendSelector_searchContainer TDFriendSelector_clearfix">
+					<div class="TDFriendSelector_selectedCountContainer"><span class="TDFriendSelector_selectedCount">0</span> / <span class="TDFriendSelector_selectedCountMax">0</span> friends selected</div>
+					<input type="text" placeholder="Search friends" id="TDFriendSelector_searchField" />
+				</div>
+				<div class="TDFriendSelector_friendsContainer"></div>
+			</div>
+			<div class="TDFriendSelector_footer TDFriendSelector_clearfix">
+				<a href="#" id="TDFriendSelector_pagePrev" class="TDFriendSelector_disabled">Previous</a>
+				<a href="#" id="TDFriendSelector_pageNext">Next</a>
+				<div class="TDFriendSelector_pageNumberContainer">
+					Page <span id="TDFriendSelector_pageNumber">1</span> / <span id="TDFriendSelector_pageNumberTotal">1</span>
+				</div>
+				<a href="#" id="TDFriendSelector_buttonOK">OK</a>
+			</div>
+		</div>
+	</div>
+</div>
 
-
+<script src="./js/tdfriendselector.js"></script>
 <script>
-$(".friendSelect").click(function () {
-  FB.ui({
-    method: 'apprequests',
-    message: 'Choose 1 friends!'
-  }, function (response) {
-    console.log(response) //The friends
+/*globals $, jQuery, FB, TDFriendSelector */
+window.fbAsyncInit = function () {
+	FB.init({appId: '<?php echo $appId; ?>', status: true, cookie: false, xfbml: false, oauth: true});
+  $(document).ready(function () {
+    var selector, logActivity, callbackFriendSelected, callbackFriendUnselected, callbackMaxSelection, callbackSubmit;
+
+    // When a friend is selected, log their name and ID
+    callbackFriendSelected = function (friendId) {
+      var friend, name;
+      friend = TDFriendSelector.getFriendById(friendId);
+      name = friend.name;
+      logActivity('Selected ' + name + ' (ID: ' + friendId + ')');
+    };
+
+    // When a friend is deselected, log their name and ID
+    callbackFriendUnselected = function (friendId) {
+      var friend, name;
+      friend = TDFriendSelector.getFriendById(friendId);
+      name = friend.name;
+      logActivity('Unselected ' + name + ' (ID: ' + friendId + ')');
+    };
+
+    // When the maximum selection is reached, log a message
+    callbackMaxSelection = function () {
+      logActivity('Selected the maximum number of friends');
+    };
+
+    // When the user clicks OK, log a message
+    callbackSubmit = function (selectedFriendIds) {
+      logActivity('Clicked OK with the following friends selected: ' + selectedFriendIds.join(", "));
+    };
+
+    // Initialise the Friend Selector with options that will apply to all instances
+    TDFriendSelector.init({
+      debug: true
+    });
+
+    // Create some Friend Selector instances
+    selector = TDFriendSelector.newInstance({
+      callbackFriendSelected: callbackFriendSelected,
+      callbackFriendUnselected: callbackFriendUnselected,
+      callbackMaxSelection: callbackMaxSelection,
+      callbackSubmit: callbackSubmit,
+      maxSelection: 1,
+      friendsPerPage: 5,
+      autoDeselection: true
+    });
+
+    FB.getLoginStatus(function (response) {
+      if (response.authResponse) {
+        $("#login-status").html("Logged in");
+        $("#btnLogin").hide();
+      } else {
+        $("#login-status").html("Not logged in");
+        $("#btnLogin").show();
+      }
+    });
+
+    $("#btnLogin").click(function (e) {
+      e.preventDefault();
+      FB.login(function (response) {
+        if (response.authResponse) {
+          console.log("Logged in");
+          $("#login-status").html("Logged in");
+          $("#btnLogin").hide();
+
+        } else {
+          console.log("Not logged in");
+          $("#login-status").html("Not logged in");
+          $("#btnLogin").show();
+        }
+      }, {});
+    });
+
+    $("#friendSelect").click(function (e) {
+      e.preventDefault();
+      selector.showFriendSelector();
+    });
+
+    logActivity = function (message) {
+      $("#results").append('<div>' + new Date() + ' - ' + message + '</div>');
+    };
   });
+};
+(function () {
+	var e = document.createElement('script');
+	e.async = true;
+	e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
+	document.getElementById('fb-root').appendChild(e);
 });
 </script>
